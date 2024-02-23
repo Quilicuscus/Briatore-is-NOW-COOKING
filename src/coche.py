39c36ff74e0175e7247e90f5694262d914e6ee3a
Jorge González <jorgegolmo@mailfence.com>
2024-02-23 21:18:22 +0100

    Archivo

--- src/coche.py
+++ src/coche.py
@@ -5,11 +5,14 @@ class Coche:
     Constantes:
     PERDIDA_GAS: tiempo extra en recorrer cada vuelta por cada litro de combustible en el depósito
     PERDIDA_RUEDAS: tiempo extra en recorrer cada vuelta por cada vuelta recorrida con las ruedas
-    MAX_GAS: capacidad del tanque de combutible
+    MAX_GAS: capacidad del tanque de combustible
 
     Atributos:
     litros_gas: litros en el tanque de combustible en un momento dado
-    vida_ruedas: vueltas recorridas por las ruedas en un momento dado
+    vida_ruedas: vueltas recorridas por las ruedas en un momento dado - Sustituir por clase Ruedas
+
+    Métodos:
+    parar: lleva el coche a boxes
     """
 
     def __init__(self, perdida_gas:float, perdida_ruedas:float, max_gas:int, litros_gas:float=-1,
@@ -28,7 +31,7 @@ class Coche:
 
 
     def parar(self, tiempo_pit:float, gas:float, ruedas:bool=True, repairs:float=0) -> float:
-        """Esta función simula una parada en boxes. Calcula el tiempo de la parada en base al tiempo de
+        """Este método simula una parada en boxes. Calcula el tiempo de la parada en base al tiempo de
         recorrer el pitlane, el combustible a repostar, si se cambian las ruedas y las posibles
         reparaciones. También resetea la degradación de las ruedas y añade el combustible al coche"""
         ##Fast repair? - A lo mejor hay que hacer algo con el tiempo de la vuelta
--- src/estrategia.py
+++ src/estrategia.py
@@ -1,90 +1,108 @@
 import itertools
+from itertools import permutations
 
 # El tiempo por vuelta sin desgaste de neumáticos es 100 segundos por vuelta
-tiempo_base = 100
+lista_tiempos_base = [100.0, 101.0, 102.0]
+# tiempo_base1 = 100
+# tiempo_base2 = 101
+# tiempo_base3 = 102
 
-# El desgaste de neumaticos etc hace que el tiempo por vuelta aumente un segundo por vuelta
-extra = 1
+# El desgaste de neumaticos etc hace que el tiempo por vuelta aumente entre 
+# 0,9 y 0,3 por vuelta en funcion del neumático
+lista_extras = [0.9,0.6,0.3]
+# extra_1 = 0.9
+# extra_2 = 0.6
+# extra_3 = 0.3
 
 # Numero de vueltas de la carrera
-vueltas = 20
+vueltas = 5
+
+
 
 # Número de paradas que pretendemos hacer durante la carrera
 numero_paradas = 2
 
-vueltas_desde_la_parada = 1
-lista_de_tiempos_de_estretegias = []
+vueltas_desde_la_parada = 0
+lista_de_tiempos_de_las_estretegias = []
 
 
-# Generar los números del 1 al 10
+# Combinatoria para encontrar todas las combinaciones de las vueltas de parada
+# en boxes
+# Generar las combinaciones del numero del 1 al 10
 numeros = list(range(1, vueltas+1))
 
-# Generar todas las combinaciones de dos números para así poder todas las posibles dos paradas
-combinaciones = list(itertools.combinations(numeros, numero_paradas))
+# lista_paradas contiene todas las tuplas con las vueltas en las que hay que 
+# parar
+lista_paradas = list(itertools.combinations(numeros, numero_paradas))
 
-# Imprimir todas las combinaciones
-for combinacion in combinaciones:
-    for i in combinacion:
-        print(i)
-    print(combinacion)
-
-for combinacion in combinaciones:
-    vuelta_actual = 1
-    tiempo_total = 0
-    tiempo_vuelta_actual = 0
-    vueltas_desde_la_parada = 1
-
-    for i in range(1,vueltas+1):
-        print("Nos encontramos en la vuelta %i" % (vuelta_actual))
-
-        if i in combinacion:
-
-            print("Esta vuelta paramos en boxes")
-            tiempo_vuelta_actual = tiempo_base + extra*(vueltas_desde_la_parada-1)
-            # Añadimos 20 segundos de la parada
-            tiempo_vuelta_actual += 20
-            print("El tiempo de esta vuelta ha sido: %i" % (tiempo_vuelta_actual))
-            tiempo_total += tiempo_vuelta_actual
-            print("Por ahora el tiempo de carrera es %i" % (tiempo_total))
-
-            vueltas_desde_la_parada = 1
-            vuelta_actual += 1
-
-        else:
-            print("Esta vuelta no paramos en boxes")
-            tiempo_vuelta_actual = tiempo_base + extra*(vueltas_desde_la_parada-1)
-            print("El tiempo de esta vuelta ha sido %i" % (tiempo_vuelta_actual))
-            tiempo_total+=tiempo_vuelta_actual
-            print("Por ahora el tiempo de carrera es %i" % (tiempo_total))
-            vueltas_desde_la_parada+=1
-            vuelta_actual+=1
-
-    print("El tiempo total de carrera ha sido %i" % (tiempo_total))
-    lista_de_tiempos_de_estretegias.append(tiempo_total)
-
-print(lista_de_tiempos_de_estretegias)
-estrategias = []
-
-# Obtenemos el tiempo de carreras mas corto
-numero_mas_pequeno = min(lista_de_tiempos_de_estretegias)
-
-# Obtenemos la estrategia que genera ese tiempo de carrera mas corto
-numero_de_estrategia = lista_de_tiempos_de_estretegias.index(numero_mas_pequeno)
-
-# Queremos obtener todas las estrategias que obtengan el mismo tiempo mínimo, aunque sea parando en vueltas distintas:
-estrategias_finales=[]
-
-for i in range(len(lista_de_tiempos_de_estretegias)):
-    if lista_de_tiempos_de_estretegias[i] == numero_mas_pequeno:
-        estrategias_finales.append(combinaciones[i])
-
-print("El tiempo de carrera mas corto posibles es: %i" % (numero_mas_pequeno))
-print("Esto se consigue con la estrategia numero %i que es la siguiente: " % (numero_de_estrategia))
-print(combinaciones[numero_de_estrategia])
-print("Estas son otras estrategias que devuelven el mismo tiempo final de carrera: ")
-print(estrategias_finales)
+# Combinatoria para hallar todas las permutaciones de los neumáticos
+numeros_neumaticos = [0, 1, 2] # 1 es blando, 2 es medio y 3 es duro
+compuesto_neumaticos = ["blando", "medio", "duro"]
 
 
+# neumaticos a poner contiene en que orden ponemos los neumaticos que 
+# tenemos que poner en el coche
+
+neumaticos_a_poner = list(permutations(numeros_neumaticos, numero_paradas+1))
+ruedas = neumaticos_a_poner[0]
+
+print(lista_paradas)
+print(neumaticos_a_poner)
+print(len(lista_paradas))
+print(len(neumaticos_a_poner))
+# Queremos por cada combinacion de vueltas en las que tenemos que parar en 
+# boxes, se prueben todas las opciones de los 3 compuestos de neumaticos
+# Por ejemplo: en lista_paradas tenemos (1,2)(1,3)(1,4).... Para cada una de 
+# ellas tenemos que probar todas las permutaciones de neumaticos: (1,2)(2,1)...
+
+for i in lista_paradas: # Para cada combinacion de vueltas de parada
+    for j in neumaticos_a_poner: # Para cada permutacion de neumaticos a poner
+        # Comienza la simulación de la carrera, así que ponemos las 
+        # condiciones iniciales:
+
+        tiempo_total = 0.0 # tiempo total de carrera
+
+        tiempo_vuelta_actual = 0.0 # tiempo de la vuelta en la que estamos
+        vueltas_desde_la_parada = 0 # Vueltas desde la ultima parada
+        numero_parada = 1 # Paradas realizadas
+        ruedas = j[0]
+        #print(ruedas)
+
+        for k in range(1,vueltas+1): # k tambien es la vuelta en la que estamos
+            print("Nos encontramos en la vuelta %i" % (k))
+            print("Esta vuelta llevamos el neumatico %s" % (compuesto_neumaticos[ruedas]))
+
+            if k in i: # Si k está en la combinacion de paradas, hay que parar
+                print("Hay que parar")
+                neumatico_que_ponemos = j[numero_parada]
+                print("ponemos el %s " % (compuesto_neumaticos[neumatico_que_ponemos]))
+                
+
+                tiempo_vuelta_actual = lista_tiempos_base[ruedas] + lista_extras[ruedas]*vueltas_desde_la_parada
+                print("El tiempo de esta vuelta ha sido %f" % (tiempo_vuelta_actual))
+
+                tiempo_total+=tiempo_vuelta_actual + 20
+                print("Por ahora el tiempo de carrera es %f" % (tiempo_total))
+
+                vueltas_desde_la_parada = 0
+                numero_parada+=1
+
+                ruedas = neumatico_que_ponemos
+
+            else:
+                print("Esta vuelta no hay que parar")
+                tiempo_vuelta_actual = lista_tiempos_base[ruedas] + lista_extras[ruedas]*vueltas_desde_la_parada
+                print("El tiempo de esta vuelta ha sido %f" % (tiempo_vuelta_actual))
+                tiempo_total+=tiempo_vuelta_actual
+                print("Por ahora el tiempo de carrera es %f" % (tiempo_total))
+                vueltas_desde_la_parada +=1
+
+        print("El tiempo total de carrera es %f " % (tiempo_total))
+        lista_de_tiempos_de_las_estretegias.append(tiempo_total)
+
+
+numero_mas_pequeno = min(lista_de_tiempos_de_las_estretegias)
+print(numero_mas_pequeno)
 
 
 
