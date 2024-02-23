42aabc19f6047cdc56a70f4b34030731fada8682
Jorge González <jorgegolmo@mailfence.com>
2024-02-23 13:28:34 +0100

    __init__ de Carrera

--- carrera.py
+++ carrera.py
@@ -1,2 +1,12 @@
+from coche import Coche
+from circuito import Circuito
+
+
+
 class Carrera:
     """Esto es como el tablero del proyecto final de progra"""
+
+    def __init__(self, perdida_gas:float, perdida_ruedas:float, max_gas:int, tiempo_base:float,
+                 ganancia_goma:float):
+        self.coche = Coche(perdida_gas, perdida_ruedas, max_gas)
+        self.circuito = Circuito(tiempo_base, ganancia_goma)
