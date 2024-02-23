62282f23575a405b64f5e1a6ef07884a5f3fb20b
Jorge González <jorgegolmo@mailfence.com>
2024-02-24 00:27:53 +0100

    Localizar .csv

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import csv
+from pathlib import Path
 
 from carrera import Carrera
 
 
 condiciones_carrera = []
 
+# Localizar input.csv (en la carpeta raíz del repositorio)
+INPUT = Path(__file__).parents[1] / "input.csv"
+
+
 # Leer input.csv, ignorar los nombres de columnas y tomar las condiciones de carrera
-with open("../input.csv") as input:
+with open(INPUT) as input:
     archivo = csv.reader(input, delimiter = ",")
     numero_fila = 0
     for fila in archivo:
