f277fab2ee31f513a927bfe7769ccdcbcf9fa90a
Quilicuscus <inigomallo2016@gmail.com>
2024-02-23 19:20:01 +0100

    Long winter, long seasons in the past, but now we can cook, we can cook

--- estrategia3.py
+++ estrategia3.py
@@ -15,14 +15,14 @@ lista_extras = [0.9,0.6,0.3]
 # extra_3 = 0.3
 
 # Numero de vueltas de la carrera
-vueltas = 20
+vueltas = 5
 
-ruedas = 2
+
 
 # Número de paradas que pretendemos hacer durante la carrera
 numero_paradas = 2
 
-vueltas_desde_la_parada = 1
+vueltas_desde_la_parada = 0
 lista_de_tiempos_de_las_estretegias = []
 
 
@@ -39,11 +39,17 @@ lista_paradas = list(itertools.combinations(numeros, numero_paradas))
 numeros_neumaticos = [0, 1, 2] # 1 es blando, 2 es medio y 3 es duro
 compuesto_neumaticos = ["blando", "medio", "duro"]
 
+
 # neumaticos a poner contiene en que orden ponemos los neumaticos que 
 # tenemos que poner en el coche
 
-neumaticos_a_poner = permutations(numeros_neumaticos, numero_paradas)
+neumaticos_a_poner = list(permutations(numeros_neumaticos, numero_paradas+1))
+ruedas = neumaticos_a_poner[0]
 
+print(lista_paradas)
+print(neumaticos_a_poner)
+print(len(lista_paradas))
+print(len(neumaticos_a_poner))
 # Queremos por cada combinacion de vueltas en las que tenemos que parar en 
 # boxes, se prueben todas las opciones de los 3 compuestos de neumaticos
 # Por ejemplo: en lista_paradas tenemos (1,2)(1,3)(1,4).... Para cada una de 
@@ -57,17 +63,20 @@ for i in lista_paradas: # Para cada combinacion de vueltas de parada
         tiempo_total = 0.0 # tiempo total de carrera
 
         tiempo_vuelta_actual = 0.0 # tiempo de la vuelta en la que estamos
-        vueltas_desde_la_parada = 1 # Vueltas desde la ultima parada
-        numero_parada = 0 # Paradas realizadas
-        ruedas = 2
+        vueltas_desde_la_parada = 0 # Vueltas desde la ultima parada
+        numero_parada = 1 # Paradas realizadas
+        ruedas = j[0]
+        #print(ruedas)
 
         for k in range(1,vueltas+1): # k tambien es la vuelta en la que estamos
+            print("Nos encontramos en la vuelta %i" % (k))
+            print("Esta vuelta llevamos el neumatico %s" % (compuesto_neumaticos[ruedas]))
 
             if k in i: # Si k está en la combinacion de paradas, hay que parar
                 print("Hay que parar")
                 neumatico_que_ponemos = j[numero_parada]
                 print("ponemos el %s " % (compuesto_neumaticos[neumatico_que_ponemos]))
-                ruedas = neumatico_que_ponemos
+                
 
                 tiempo_vuelta_actual = lista_tiempos_base[ruedas] + lista_extras[ruedas]*vueltas_desde_la_parada
                 print("El tiempo de esta vuelta ha sido %f" % (tiempo_vuelta_actual))
@@ -75,8 +84,10 @@ for i in lista_paradas: # Para cada combinacion de vueltas de parada
                 tiempo_total+=tiempo_vuelta_actual + 20
                 print("Por ahora el tiempo de carrera es %f" % (tiempo_total))
 
-                vueltas_desde_la_parada = 1
+                vueltas_desde_la_parada = 0
                 numero_parada+=1
+
+                ruedas = neumatico_que_ponemos
 
             else:
                 print("Esta vuelta no hay que parar")
